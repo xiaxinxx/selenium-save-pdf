434d6987efac209d8a699602c922d1d1cea70f60
xiaxin05 <xiaxin05@meituan.com>
2021-08-31 19:26:25 +0800

    rename saved pdf

--- src/main/java/crawler/PageProcessor.java
+++ src/main/java/crawler/PageProcessor.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -40,24 +41,24 @@ public class PageProcessor {
         WebDriver driver = new ChromeDriver(options);
 
         //登录
-        driver.get("https://www.baidu.com/");  //替换为登录地址
+        //driver.get("https://www.baidu.com/");  //替换为登录地址
 
-        //文本输入的例如
-        WebElement element_text = driver.findElement(By.xpath("//*[@id=\"kw\"]"));
-        element_text.sendKeys("苍井空");
+        ////文本输入的例如
+        //WebElement element_text = driver.findElement(By.xpath("//*[@id=\"kw\"]"));
+        //element_text.sendKeys("苍井空");
+        //
+        ////按钮点击的例子
+        //WebElement element_btn = driver.findElement(By.xpath("//*[@id=\"su\"]"));
+        //element_btn.click();
+        //
+        //doSleep();
 
-        //按钮点击的例子
-        WebElement element_btn = driver.findElement(By.xpath("//*[@id=\"su\"]"));
-        element_btn.click();
-
-        doSleep();
-
-        String s = "http://tensent.com/AppStore/Wiki/Pages/WikiArticles.html?id=";
+        //String s = "http://tensent.com/AppStore/Wiki/Pages/WikiArticles.html?id=";
 
         //开始存储页面
-        for (int i = 1; i <= 36000; i++) {
-            String realURL = s + i +"";
-            print(driver, realURL);
+        for (int i = 1; i <= 3; i++) {
+
+            print1(driver, "realURL" + i);
         }
 
         driver.quit();
@@ -75,6 +76,22 @@ public class PageProcessor {
         js.executeScript("window.print();");
 
         doSleep();
+        rename(element_title.getText());
+    }
+
+    public static void print1(WebDriver driver, String url) {
+        driver.get("https://www.baidu.com/");
+        //加逻辑判断是否为无效页面
+        //WebElement element_title = driver.findElement(By.id("article-title"));
+        //if (element_title.getText().contains("文章不存在")) {
+        //    return;
+        //}
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.print();");
+
+        doSleep();
+        rename(url);
     }
 
     public static void doSleep() {
@@ -83,5 +100,14 @@ public class PageProcessor {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void rename(String name) {
+        String user_dir = System.getProperty("user.dir");
+        String old_name = user_dir + "/pdf/百度一下，你就知道.pdf";
+        String new_name = user_dir + "/pdf/" + name + ".pdf";
+        File file = new File(old_name);
+        File file2 = new File(new_name);
+        file.renameTo(file2);
     }
 }
